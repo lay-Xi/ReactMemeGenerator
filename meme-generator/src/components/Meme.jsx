000652c0db3b50b91f39595ca5220662d22496fb
Xi Gao <xiga6451@colorado.edu>
2022-03-18 20:23:56 -0600

    Added way to show new image on button click

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,17 +1,29 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import memeData from '../memeData.js';
+import { useState } from 'react';
 
 export default function Meme() {
+  const [imageUrl, setImage] = useState('');
+
+  function getRandomImage() {
+    const memeArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+
+    setImage(memeArray[randomNumber].url);
+  }
+
   return (
     <main>
-      <form className='form'>
+      <div className='form'>
         <input className='form--input' placeholder='Top Text'></input>
         <input className='form--input' placeholder='Bottom Text'></input>
-        <button className='form--button'>
+        <button className='form--button' onClick={getRandomImage}>
           Get a new meme image &nbsp;
           <FontAwesomeIcon icon={solid('image')} />
         </button>
-      </form>
+      </div>
+      <img className='meme--image' src={imageUrl} alt='' />
     </main>
   );
 }
